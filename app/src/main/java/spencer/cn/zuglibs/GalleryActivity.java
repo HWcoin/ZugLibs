4c6dfa26184b5d791dccf8844d398867b5c99b6f
spencer <1096025179@qq.com>
2016-10-18 10:12:15 +0800

    add Gallery Demo (modified demo)

--- app/src/main/java/spencer/cn/zuglibs/GalleryActivity.java
+++ app/src/main/java/spencer/cn/zuglibs/GalleryActivity.java
@@ -7,6 +7,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -29,7 +30,7 @@ public class GalleryActivity extends AppCompatActivity {
             public void onClick(View v) {
                 //注意MediaStore.Images.Media.EXTERNAL_CONTENT_URI、MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
                 //以及Uri如何转变为InputStream
-                startActivity(new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI));
+                startActivityForResult(new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI), 234);
             }
         });
     }
@@ -37,11 +38,12 @@ public class GalleryActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (data.getAction() == Intent.ACTION_PICK){
+        if (resultCode == RESULT_OK){
             Uri imageFileUri = data.getData();
-
+            Log.e("sos", data.getAction()+":"+Intent.ACTION_PICK);
             try {
                 Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageFileUri));
+//                Bitmap b = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight());
                 showGallery.setImageBitmap(bitmap);
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
